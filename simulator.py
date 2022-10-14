35d42ab002cc4865ead03067a9da227fd373f631
Romil <romil.bhardwaj@gmail.com>
2022-10-14 11:07:10 -0700

    Simulator

--- simulator.py
+++ simulator.py
@@ -38,6 +38,7 @@ class Region:
         # p is preemption probability for a job in this region at every tick
         self.name = name
         self.jobs = []
+        # TODO: Preemption probability could also be a function of the number of jobs in the region
         self.p = p
         self.history = []
 
@@ -65,30 +66,54 @@ class Scheduler:
         self.jobs = []
         self.policy_fn = self.get_policy(policy)
 
-    def get_policy(self, policy_str):
+    def get_policy(self, policy_str) -> callable:
         if policy_str == 'random':
             return self._policy_random
         elif policy_str == 'least_loaded':
             return self._policy_least_loaded
+        elif policy_str == 'epsilon_greedy':
+            return self._policy_epsilon_greedy
         else:
             raise ValueError(f"Unknown policy: {policy_str}")
 
     def add_job(self, job):
         self.jobs.append(job)
 
-    def _policy_random(self, regions):
+    def _policy_random(self) -> Region:
         # Randomly schedule a job to a region
-        return random.choice(regions)
+        return random.choice(self.regions)
 
-    def _policy_least_loaded(self, job, regions):
+    def _policy_epsilon_greedy(self) -> Region:
+        # Schedule a job to the region that has been the most successful, with probability 1-epsilon to schedule to a random region
+        # TODO - Change this hardcoded value to be a policy parameter
+        epsilon = 0.3
+        best_region = None
+        longest_running_time = 1
+        # Apply epsilon-greedy exploration
+        if random.random() < epsilon:
+            best_region = self._policy_random()
+            print("Randomly exploring a new region: ", best_region)
+            return best_region
+        else:
+            for job in self.jobs:
+                if job.region_running_time > longest_running_time:
+                    best_region = job.region
+                    longest_running_time = job.region_running_time
+            if best_region is None:
+                # If no jobs have been running, randomly choose a region
+                best_region = self._policy_random()
+            print("Most successful job and region: ", job, best_region)
+            return best_region
+
+    def _policy_least_loaded(self) -> Region:
         # Schedule a job to the least loaded region
-        return min(regions, key=lambda r: len(r.jobs))
+        return min(self.regions, key=lambda r: len(r.jobs))
 
     def run_scheduler(self):
         for job in self.jobs:
             if job.region is None:
                 # Job is not running, schedule it
-                job.set_region(self.policy_fn(self.regions))
+                job.set_region(self.policy_fn())
                 job.region.add_job(job)
                 global CLOCK
                 print(f"t={CLOCK}: Scheduled {job} to {job.region}")
